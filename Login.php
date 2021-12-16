f85af93b6a44854cfd0c3bfb4539c00730d8c8c0
12200120dinisapika <91046627+12200120dinisapika@users.noreply.github.com>
2021-12-17 01:45:06 +0700

    file controller dan filter

---
+++ Login.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\PenggunaModel;
+use Config\Services;
+
+class Login extends BaseController{
+
+
+    public function cekLogin(){
+        $email = $this->request->getPost('email');
+        $sandi = $this->request->getPost('sandi');
+
+        $v = $this->validate([
+            'email' => 'required',
+            'sandi' => 'required'
+        ], [
+            'email'=>[
+                'required' => 'Email tidak boleh kosong',
+            ],
+            'sandi'=> [
+                'required' => 'Sandi tidak boleh kosong'
+            ]
+        ]);
+
+        $this->session->set('email', $email);
+        $this->session->set('sandi', $sandi);
+
+        if( $v == false ){
+            $this->session->setFlashdata('validator', $this->validator);
+            return redirect()->to('/login');
+        }else{
+
+            $vl = (new PenggunaModel_12200144())->cekLogin($email, $sandi);
+            
+            if($vl == null){
+                return redirect()->to('/login')->with(' error', 'User dan sandi salah');
+            }else{ 
+                $this->session->set('sudahLogin', true); 
+                return redirect()->to('/beranda');
+            }
+        }
+    }
+
+    public function beranda(){
+
+    }
+}
